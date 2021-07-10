b3a6f30c768a7b22b0ecfa08e28b6a587188da30
Hugo Fitas <hfitas@gmail.com>
2021-07-10 02:50:17 +0100

    newUser was returning an array instead of an object

--- routes/users.route.js
+++ routes/users.route.js
@@ -13,7 +13,7 @@ router.post("/", (req, res) => {
 
       return db.query("SELECT * FROM users WHERE id = ?", [newUserId]);
     })
-    .then(([newUser]) => res.status(200).json(newUser))
+    .then(([newUser]) => res.status(200).json(newUser[0]))
     .catch((err) => res.status(500).json(err));
 });
 
